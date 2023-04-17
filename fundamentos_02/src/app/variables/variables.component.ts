56a1b9ed0e0a1f3bf4481b203e87bc360c2f3927
AdriElvis <adriane.sanchez@softtek.com>
2023-04-17 13:26:52 +0200

    terminado f_02

--- fundamentos_02/src/app/variables/variables.component.ts
+++ fundamentos_02/src/app/variables/variables.component.ts
@@ -15,13 +15,19 @@ export class VariablesComponent {
   imprimirTipos()
   {
     let numero:number = 1;
-    let cadena:string = "Cadena de texto";
+    let cadena1:string = "Cadena de texto con comillas dobles";
+    let cadena2:string = 'Cadena de texto con comillas simples';
+    let nombre:string = 'Adrián'
+    let trabajo:string = 'estudiante'
+    let cadena3:string = `${nombre} es un ${trabajo}`
     let booleano:boolean = true;
     let cualquiera:any =75;
     let desconocido:unknown="tipo desconocido";
 
     console.log("El tipo de variable es ",typeof numero, " y el valor es ",numero);
-    console.log("El tipo de variable es ",typeof cadena, " y el valor es ",cadena);
+    console.log("El tipo de variable es ",typeof cadena1, " y el valor es ",cadena1);
+    console.log("El tipo de variable es ",typeof cadena2, " y el valor es ",cadena2);
+    console.log("El tipo de variable es ",typeof cadena3, " y el valor es ",cadena3);
     console.log("El tipo de variable es ",typeof booleano, " y el valor es ",booleano);
     console.log("El tipo de variable es ",typeof cualquiera, " y el valor es ",cualquiera);
     console.log("El tipo de variable es ",typeof desconocido, " y el valor es ",desconocido);
