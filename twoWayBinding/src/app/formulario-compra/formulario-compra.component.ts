49b07376b1c3aea0d9f683ee091a6e2f9ab7bb4b
AdriElvis <adriane.sanchez@softtek.com>
2023-04-17 13:27:28 +0200

    terminado twoWayBinding

--- twoWayBinding/src/app/formulario-compra/formulario-compra.component.ts
+++ twoWayBinding/src/app/formulario-compra/formulario-compra.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario-compra.component.css']
 })
 export class FormularioCompraComponent {
+  correo:string=""
+  direccion:string=""
+  telefono:string=""
+  comunidad:string=""
+  pago:string=""
+  acepta:boolean=false
+  info:string=""
 
+  mostrar(){
+    this.info="Correo: "+this.correo+
+    "\nDirección: "+this.direccion+
+    "\nTelefono:"+ this.telefono+
+    "\nComunidad:"+ this.comunidad+
+    "\nForma de pago:"+ this.pago+
+    "\nAcepta Recibir información de nuestros productos:"+(this.acepta?"sí":"no")
+  }
 }
+
