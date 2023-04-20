28863823f0f0c8859056203bf22c0ad0f2b2021c
AdriElvis <adriane.sanchez@softtek.com>
2023-04-20 23:42:39 +0200

    En proceso

--- miPagina_bootstrap/src/app/app-routing.module.ts
+++ miPagina_bootstrap/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { InicioComponent } from './inicio/inicio.component';
+import { FormContactoComponent } from './form-contacto/form-contacto.component';
+const routes: Routes = [
+  {path: "", component: InicioComponent},
+  {path: "form_contacto", component: FormContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- miPagina_bootstrap/src/app/app.module.ts
+++ miPagina_bootstrap/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormContactoComponent } from './form-contacto/form-contacto.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    FormContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
